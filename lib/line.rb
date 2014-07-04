8a0b46b5b42ae8e4920a86bb3837a09054f9c7d8
Sargam <sera.bravo004@gmail.com>
2014-07-04 11:22:35 +0530

    specification for unequal coordinates passed

--- lib/line.rb
+++ lib/line.rb
@@ -1,11 +1,19 @@
 # returns length of line
 class Line
   def initialize (x1, y1, x2, y2)
-
+    @x1 = x1
+    @y1 = y1
+    @x2 = x2
+    @y2 = y2
   end
 
   def length
-    0
+    #if @x1==@x2 && @y1==@y2
+    if [@x1,@y1] == [@x2,@y2]
+      0
+    else
+      1
+    end
   end
-  
+
 end
